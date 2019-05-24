49443f0f8605dbc7d57c273ed692dc38d17228be
Graisvendir <graisvendir@gmail.com>
2019-05-24 12:39:30 +0600

    refactor tests

--- features/music.feature
+++ features/music.feature
@@ -3,5 +3,13 @@ Feature: Music
 	Scenario: try to win
 		Given I have guess the music game
 		And I have one music in variants
+		And I have current music
 		When I have entered number '3' as number of melody
 		Then I lose the game
+
+	Scenario: try to win
+		Given I have guess the music game
+		And I have one music in variants
+		And I have current music
+		When I have entered number '1' as number of melody
+		Then I win the game
--- features/steps/Music.py
+++ features/steps/Music.py
@@ -15,13 +15,29 @@ def step_impl(context):
     global pool
     pool.add_variant('AC/DC - Back In Black')
 
+@given('I have current music')
+def step_impl(context):
+    global pool
+    pool.set_random_current_music()
+
 @when('I have entered number \'3\' as number of melody')
 def step_impl(context):
     global choice
     choice = 3
+
+@when('I have entered number \'1\' as number of melody')
+def step_impl(context):
+    global choice
+    choice = 1
 
 @then('I lose the game')
 def step_impl(context):
     global pool
     global choice
     assert pool.check_choice(choice) is 0
+
+@then('I win the game')
+def step_impl(context):
+    global pool
+    global choice
+    assert pool.check_choice(choice) is 1
